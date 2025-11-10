e2c9ddea082b13a1e5da243c2754f18be606821a
Iryna_Novikova <iris_vs@ukr.net>
2025-11-10 02:31:29 +0200

    some changes

---
+++ components/AuthProvider/AuthProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+//зімінити на вірні маршрути
+// import { getSession, getUserProfile } from '../../lib/api/clientApi';
+// import { useUserAuthStore } from '../../lib/store/authStore';
+// import { useEffect } from 'react';
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  // const setAuth = useUserAuthStore(state => state.setUser);
+  // const clearIsAuth = useUserAuthStore(state => state.clearIsAuthenticated);
+
+  // useEffect(() => {
+  //   const fetchSession = async () => {
+  //     const response = await getSession();
+  //     if (response) {
+  //       const user = await getUserProfile();
+  //       setAuth(user);
+  //     } else {
+  //       clearIsAuth();
+  //     }
+  //   };
+  //   fetchSession();
+  // }, [setAuth, clearIsAuth]);
+
+  return children;
+};
+
+export default AuthProvider;
