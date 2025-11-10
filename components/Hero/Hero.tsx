17656a08ee912c9d0a3c51d96073a96de1c85492
Andrii <simonandrij717@gmail.com>
2025-11-10 13:34:15 +0100

    complete hero section, add styles on app-page for backgroud, add background images to public folder

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,3 +1,19 @@
+import css from "./Hero.module.css";
+
 export default function Hero() {
-  return <div> Hero component for home page </div>;
+  return (
+    <div className={css.hero}>
+      <div className={css.hero_text}>
+        <h1 className={css.title}>Відкрийте світ подорожей з нами!</h1>
+        <p className={css.text}>
+          Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися
+          своїми історіями та отримувати натхнення для нових пригод. Відкрийте
+          для себе нові місця та знайдіть однодумців!
+        </p>
+      </div>
+      <a href="#Join" className={css.button}>
+        Доєднатись
+      </a>
+    </div>
+  );
 }
