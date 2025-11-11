71c40dc01a84cbce750d81e5de140f3bae6bcbf3
olesia <ldsolesya@gmail.com>
2025-11-11 13:11:24 +0000

    add TravellerInfoCard component

--- components/TravellerInfoCard/TravellerInfoCard.tsx
+++ components/TravellerInfoCard/TravellerInfoCard.tsx
@@ -1,3 +1,33 @@
-export default function TravellerInfoCard() {
-  return <header> TravellerInfoCard </header>;
+import Link from "next/link";
+import Image from "next/image";
+import css from "./TravellerInfoCard.module.css";
+
+type TravellerInfoCardProps = {
+  id: string;
+  name: string;
+  description: string;
+  photoUrl: string;
+};
+
+export default function TravellerInfoCard({
+  id,
+  name,
+  description,
+  photoUrl,
+}: TravellerInfoCardProps) {
+  return (
+    <div className={css.card}>
+      <div className={css.imageWrapper}>
+        <Image src={photoUrl} alt={name} className={css.avatar} />
+      </div>
+
+      <div className={css.content}>
+        <h3 className={css.name}>{name}</h3>
+        <p className={css.description}>{description}</p>
+        <Link href={`/travellers/${id}`} className={css.profileButton}>
+          Переглянути профіль
+        </Link>
+      </div>
+    </div>
+  );
 }
