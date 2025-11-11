f29786e20c5b159debc2d410fdbd526bb9a6557d
olesia <ldsolesya@gmail.com>
2025-11-11 12:32:09 +0000

    add Header component and AuthNavigation

--- components/AuthNavigation/AuthNavigation.tsx
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -1,3 +1,85 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useUserAuthStore } from "@/lib/api/store/authStore";
+// import { logOut } from "@/lib/api/clientApi";
+import css from "./AuthNavigation.module.css";
+
 export default function AuthNavigation() {
-  return <div> I need for header </div>;
+  const { isAuthenticated, user, clearIsAuthenticated } = useUserAuthStore();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      // await logOut();
+    } catch (error) {
+      console.error("Logout error:", error);
+    } finally {
+      clearIsAuthenticated();
+      router.replace("/login");
+    }
+  };
+
+  if (isAuthenticated) {
+    return (
+      <ul className={css.nav_list}>
+        <li>
+          <Link href="/">Головна</Link>
+        </li>
+        <li>
+          <Link href="/stories">Історії</Link>
+        </li>
+        <li>
+          <Link href="/travellers">Мандрівники</Link>
+        </li>
+        <li>
+          <Link href="/profile">Мій Профіль</Link>
+        </li>
+        <li>
+          <Link href="/stories/create">Опублікувати історію</Link>
+        </li>
+        <li>
+          <Link href="/profile" className={css.profile_link}>
+            {user?.avatarUrl ? (
+              <img
+                src={user.avatarUrl}
+                alt={user.name || "User"}
+                className={css.avatar}
+              />
+            ) : (
+              <span>{user?.name}</span>
+            )}
+          </Link>
+        </li>
+        <li>
+          <button onClick={handleLogout} className={css.logout_btn}>
+            <svg width={24} height={24}>
+              <use href="/svg-sprite.svg#icon-logout"></use>
+            </svg>
+          </button>
+        </li>
+      </ul>
+    );
+  }
+
+  return (
+    <ul className={css.nav_list}>
+      <li>
+        <Link href="/">Головна</Link>
+      </li>
+      <li>
+        <Link href="/stories">Історії</Link>
+      </li>
+      <li>
+        <Link href="/travelers">Мандрівники</Link>
+      </li>
+      <li>
+        <Link href="/login">Вхід</Link>
+      </li>
+      <li>
+        <Link href="/register">Реєстрація</Link>
+      </li>
+    </ul>
+  );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import { useUserAuthStore } from "@/lib/api/store/authStore";
+import AuthNavigation from "../AuthNavigation/AuthNavigation";
+import css from "./Header.module.css";
+
 export default function Header() {
-  return <header> HEADER </header>;
+  const { isAuthenticated } = useUserAuthStore();
+
+  return (
+    <header className={css.container}>
+      <div className={css.header_container}>
+        <div className={css.logo_container}>
+          <Link href="/" className={css.logo}>
+            <svg width={22} height={22}>
+              <use href="/svg-sprite.svg#icon-logo"></use>
+            </svg>
+            <svg width={123} height={20}>
+              <use href="/svg-sprite.svg#icon-logo-text"></use>
+            </svg>
+          </Link>
+        </div>
+
+        {/*навігація планшет/мобілка*/}
+        <div className={css.nav_tablet}>
+          {isAuthenticated && (
+            <Link
+              href="/stories/create/page.tsx"
+              className={css.create_story_btn}
+            >
+              Опублікувати історію
+            </Link>
+          )}
+
+          <button className={css.burger_btn}>
+            <div className={css.btn_background}>
+              <svg width={24} height={24} className={css.burger_icon}>
+                <use href="/svg-sprite.svg#icon-burger-menu"></use>
+              </svg>
+            </div>
+          </button>
+        </div>
+
+        {/* Навигація десктоп */}
+        <nav className={css.nav_desktop}>
+          <AuthNavigation />
+        </nav>
+      </div>
+    </header>
+  );
 }
