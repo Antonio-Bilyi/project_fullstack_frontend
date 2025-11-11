3bc113d6c97aa15efcdf8dcb56f1db36b376bc27
ShumyloYana <zolotushko1@gmail.com>
2025-11-11 13:09:57 +0200

    add FormRegister and FormLogin

---
+++ app/(auth routes)/auth/layout.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
+import { checkSession } from '@/lib/api/clientsApi/clientApi';
+import { useUserAuthStore } from '@/lib/api/store/authStore';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function PublicLayout({ children }: Props) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const pathname = usePathname();
+  const clearIsAuthenticated = useUserAuthStore((state) => state.clearIsAuthenticated);
+  const setUser = useUserAuthStore((state) => state.setUser);
+
+  useEffect(() => {
+    const verifySession = async () => {
+      try {
+        const isAuthenticated = await checkSession(); 
+        if (isAuthenticated) {
+          
+          if (pathname.startsWith('/auth')) {
+            router.push('/');
+          }
+        } else {
+          clearIsAuthenticated(); 
+        }
+      } catch (error) {
+        console.error('Session check failed:', error);
+        clearIsAuthenticated();
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    verifySession();
+  }, [router, pathname, setUser, clearIsAuthenticated]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen text-xl font-medium">
+        Loading...
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+}
