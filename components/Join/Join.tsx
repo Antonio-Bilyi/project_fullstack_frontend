09ebabde77a944fef4b7bb60c27f1a5bad7f8821
unknown <dmytrenkoalyona@gmail.com>
2025-11-12 20:29:09 +0200

    update join and join css

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -1,57 +1,38 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import css from '../Join/Join.module.css';
-import { useUserAuthStore } from '@/lib/store/authStore';
+"use client";
 
+import { useRouter } from "next/navigation";
+import Section from "@/components/Section/Section";
+import Container from "@/components/Container/Container";
+import css from "./Join.module.css";
+import { useUserAuthStore } from "@/lib/store/authStore";
 
 export default function Join() {
   const router = useRouter();
-  const { isAuthenticated, setUser, clearIsAuthenticated } = useUserAuthStore();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`, {
-          method: 'POST',
-          credentials: 'include',
-        });
-        if (!res.ok) throw new Error('Not authorized');
-        const data = await res.json();
-
-        if (data.user) {
-          setUser(data.user);
-        } else {
-          clearIsAuthenticated();
-        }
-      } catch {
-        clearIsAuthenticated();
-      }
-    };
-
-    fetchUser();
-  }, [setUser, clearIsAuthenticated]);
+  const { isAuthenticated } = useUserAuthStore();
 
   const handleClick = () => {
     if (isAuthenticated) {
-      router.push('/profile');
+      router.push("/profile");
     } else {
-      router.push('/auth/register');
+      router.push("/auth/register");
     }
   };
 
   return (
-    <section className={css.join}>
-      <div className={css.container}>
-        <h2 className={css.title}>Приєднуйтесь до нашої спільноти</h2>
-        <p className={css.text}>
-          Долучайтеся до мандрівників, які діляться своїми історіями та надихають на нові пригоди.
-        </p>
-
-        <button onClick={handleClick} className={css.button}>
-          {isAuthenticated ? 'Збережені' : 'Зареєструватися'}
-        </button>
-      </div>
-    </section>
+    <Section>
+      <Container classes={["join", "joinBackground"]}>
+        <div className={css.join}>
+          <h2 className={css.title}>Приєднуйтесь до нашої спільноти</h2>
+          <p className={css.text}>
+            Долучайтеся до мандрівників, які діляться своїми історіями та надихають на нові пригоди.
+          </p>
+          <button className={css.button} onClick={handleClick}>
+            {isAuthenticated ? "Перейти в профіль" : "Зареєструватися"}
+          </button>
+        </div>
+      </Container>
+    </Section>
   );
 }
+
+
