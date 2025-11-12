e8d378e97529622dffad5c3c7062c2ac851fc094
Anton <anthonybelyj@protonmail.com>
2025-11-12 23:02:55 +0200

    add footer

--- components/AuthNavigationFooter/AuthNavigationFooter.tsx
+++ components/AuthNavigationFooter/AuthNavigationFooter.tsx
@@ -1,3 +1,23 @@
+import Link from 'next/link';
+import css from './AuthNavigationFooter.module.css';
+
+
 export default function AuthNavigation() {
-  return <div> I need for footer AuthNavigation </div>;
-}
+  return (
+    <nav aria-label='Footer-Navigation'>
+      <ul className={css.footer_navigation_list}>
+        <li className={css.footer_navigation_list_item}>
+          <Link href='/'>Головна</Link>
+        </li>
+
+        <li className={css.footer_navigation_list_item}>
+          <Link href='/stories'>Історії</Link>
+        </li>
+
+        <li className={css.footer_navigation_list_item}>
+          <Link href='/travellers'>Мандрівники</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+};
