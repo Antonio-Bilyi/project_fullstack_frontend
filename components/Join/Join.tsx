34a670675d9d5865261beed6fa76dc935e25e66d
AlyonaDmytrenko <143014363+AlyonaDmytrenko@users.noreply.github.com>
2025-11-14 19:46:07 +0200

    Add JOIN component and container styles

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Section from "@/components/Section/Section";
+import Container from "@/components/Container/Container";
+import css from "./Join.module.css";
+import { useUserAuthStore } from "@/lib/store/authStore";
+
 export default function Join() {
-  return <div> Join component for home page </div>;
+  const router = useRouter();
+  const { isAuthenticated } = useUserAuthStore();
+
+  const handleClick = () => {
+    if (isAuthenticated) {
+      router.push("/profile");
+    } else {
+      router.push("/auth/register");
+    }
+  };
+
+  return (
+    <Section>
+      <Container classes={["join", "joinBackground"]}>
+        <div className={css.join}>
+          <h2 className={css.title}>Приєднуйтесь до нашої спільноти</h2>
+          <p className={css.text}>
+            Долучайтеся до мандрівників, які діляться своїми історіями та надихають на нові пригоди.
+          </p>
+          <button className={css.button} onClick={handleClick}>
+            {isAuthenticated ? "Перейти в профіль" : "Зареєструватися"}
+          </button>
+        </div>
+      </Container>
+    </Section>
+  );
 }
+
+
