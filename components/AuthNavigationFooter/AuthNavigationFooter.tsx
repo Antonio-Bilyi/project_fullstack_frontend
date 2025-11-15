eb8ef842439f3681662ee8c6d9b7b4727ee63d09
Anton <anthonybelyj@protonmail.com>
2025-11-15 14:40:44 +0200

    add hover effects and route logic

--- components/AuthNavigationFooter/AuthNavigationFooter.tsx
+++ components/AuthNavigationFooter/AuthNavigationFooter.tsx
@@ -1,21 +1,42 @@
-import Link from 'next/link';
+'use client'
+
 import css from './AuthNavigationFooter.module.css';
+import { useRouter } from 'next/navigation';
+import { useUserAuthStore } from "@/lib/store/authStore";
+
 
 
 export default function AuthNavigation() {
+  const router = useRouter();
+  const isAuthenticated = useUserAuthStore(state => state.isAuthenticated);
+
+  const handleNavClick = (href: string) => {
+    if (!isAuthenticated) {
+      router.push('/auth/register');
+    } else {
+      router.push(href);
+    }
+  };
+
   return (
     <nav aria-label='Footer-Navigation'>
       <ul className={css.footer_navigation_list}>
-        <li className={css.footer_navigation_list_item}>
-          <Link href='/'>Головна</Link>
+        <li
+          className={css.footer_navigation_list_item}
+          onClick={() => handleNavClick('/')}>
+          Головна
         </li>
 
-        <li className={css.footer_navigation_list_item}>
-          <Link href='/stories'>Історії</Link>
+        <li
+          className={css.footer_navigation_list_item}
+          onClick={() => handleNavClick('/stories')}>
+          Історії
         </li>
 
-        <li className={css.footer_navigation_list_item}>
-          <Link href='/travellers'>Мандрівники</Link>
+        <li
+          className={css.footer_navigation_list_item}
+          onClick={() => handleNavClick('/travellers')}>
+          Мандрівники
         </li>
       </ul>
     </nav>
