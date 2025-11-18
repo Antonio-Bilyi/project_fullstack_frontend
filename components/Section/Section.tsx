4afac3cc590b8df8a5a9b1dd4000b299c2bca04e
Anton <anthonybelyj@protonmail.com>
2025-11-18 12:54:17 +0200

    fOOxed

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -5,9 +5,10 @@ import css from "@/components/Section/Section.module.css";
 interface SectionProps {
   children: React.ReactNode;
   classes?: string[]; // додаткові класи
+  idSection?: string;
 }
 
-const Section = ({ children, classes = [] }: SectionProps) => {
+const Section = ({ children, classes = [], idSection }: SectionProps) => {
   const allClasses = [
     css.section, // обов'язковий клас
     ...classes.map((cl) => css[cl] ?? cl), // мапимо модульні класи
@@ -16,7 +17,7 @@ const Section = ({ children, classes = [] }: SectionProps) => {
   const className = allClasses.join(" ");
 
   // console.log("SectionClassName: ", className);
-  return <section className={className}> {children} </section>;
+  return <section className={className} id={idSection}> {children} </section>;
 };
 
 export default Section;
