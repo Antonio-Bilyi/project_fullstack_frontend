8855f3e50ff79a2291183c96a602e90fcc6e20b3
Iryna_Novikova <iris_vs@ukr.net>
2025-11-18 15:09:34 +0200

    styles fix, add Container to header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,23 +3,26 @@
 import Link from "next/link";
 import css from "./Header.module.css";
 import HeaderRight from "./HeaderRight";
+import Container from "../Container/Container";
 
 export default function Header({ isHome = false }) {
   return (
     <header className={isHome ? css.homeContainer : css.defaultContainer}>
-      <div className={css.header_container}>
-        <div className={css.logo_container}>
-          <Link href="/" className={css.logo}>
-            <svg width={22} height={22}>
-              <use href="/svg-sprite.svg#icon-logo"></use>
-            </svg>
-            <p className={isHome ? css.homeLogoText : css.defaultLogoText}>
-              Подорожники
-            </p>
-          </Link>
+      <Container>
+        <div className={css.header_container}>
+          <div className={css.logo_container}>
+            <Link href="/" className={css.logo}>
+              <svg width={22} height={22}>
+                <use href="/svg-sprite.svg#icon-logo"></use>
+              </svg>
+              <p className={isHome ? css.homeLogoText : css.defaultLogoText}>
+                Подорожники
+              </p>
+            </Link>
+          </div>
+          <HeaderRight isHome={isHome} />
         </div>
-        <HeaderRight isHome={isHome} />
-      </div>
+      </Container>
     </header>
   );
 }
